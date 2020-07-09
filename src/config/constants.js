19296ae183dae70e16f15b5c611353bde1f9f0a1
Djimovanberlo <djimovanberlo@gmail.com>
2020-07-09 17:31:47 +0200

    test with heroku URL

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,2 +1,5 @@
-export const apiUrl = process.env.API_URL || "localhost:4000/graphql";
+export const apiUrl =
+  process.env.API_URL ||
+  "https://soaring-flocks.herokuapp.com/ " ||
+  "localhost:4000/graphql";
 export const DEFAULT_MESSAGE_TIMEOUT = 3000;
