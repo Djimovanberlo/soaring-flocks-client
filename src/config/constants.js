23c76d940f4dbc80f616a37ac2678d455a46ff07
Djimovanberlo <djimovanberlo@gmail.com>
2020-07-09 17:34:41 +0200

    update URL for heroku

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,5 +1,4 @@
 export const apiUrl =
-  process.env.API_URL ||
-  "https://soaring-flocks.herokuapp.com/ " ||
-  "localhost:4000/graphql";
+  process.env.API_URL || "https://soaring-flocks.herokuapp.com/ ";
+//   "localhost:4000/graphql";
 export const DEFAULT_MESSAGE_TIMEOUT = 3000;
