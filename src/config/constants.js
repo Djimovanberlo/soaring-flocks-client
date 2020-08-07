4505325fbb373ff099d948dfa43ba1be48cc2428
Djimovanberlo <djimovanberlo@gmail.com>
2020-08-07 11:16:50 +0200

    fixed connect URL to heroku

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,5 +1,5 @@
 export const apiUrl =
   process.env.API_URL ||
-  // "soaring-flocks.herokuapp.com/graphql" ||
+  "soaring-flocks.herokuapp.com/graphql" ||
   "localhost:4000/graphql";
 export const DEFAULT_MESSAGE_TIMEOUT = 3000;
